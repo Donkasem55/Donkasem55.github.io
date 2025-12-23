4b24055f715c1f22fa4dc880e86200d74a4058ab
Kasemtammakij Tanadon <h2510008@nushigh.edu.sg>
2025-12-23 17:43:55 +0800

    update

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Happy developing ✨')
+function toggleVisibility() {
+    const x = document.getElementById("find");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
